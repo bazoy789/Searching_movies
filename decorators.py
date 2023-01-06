a6254a60cf222fd19029a51459eeecab94b25d67
bazoy <bazoy@yandex.ru>
2023-01-06 23:57:02 +0300

    add func

---
+++ decorators.py
@@ -0,0 +1,40 @@
+from flask_restx import abort
+from flask import request
+import jwt
+
+from constants import JWT_SECRET, JWT_ALGORITHM
+
+
+def auth_req(func):
+    def wrapper(*args, **kwargs):
+        if 'Authorization' in request.headers:
+            abort(401)
+
+        data = request.headers['Authorization']
+        token = data.split('Bearer ')[-1]
+        try:
+            jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        except Exception:
+            abort(401)
+
+        return func(*args, **kwargs)
+    return wrapper
+def admin_req(func):
+    def wrapper(*args, **kwargs):
+        if 'Authorization' in request.headers:
+            abort(401)
+
+        data = request.headers['Authorization']
+        token = data.split('Bearer ')[-1]
+        try:
+            decode_token = jwt.decode(token, JWT_SECRET, algorithms=JWT_ALGORITHM)
+
+            role = decode_token.get('role')
+
+            if role != 'admin':
+                abort(401)
+        except Exception:
+            abort(401)
+
+        return func(*args, **kwargs)
+    return wrapper
